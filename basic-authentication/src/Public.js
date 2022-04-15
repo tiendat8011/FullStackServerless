f25f1510ddba823c63e897b25355ebdcb8ec03c8
Dat <ntd8011@gmail.com>
2022-04-15 16:00:55 +0700

    chapter4: authentication

---
+++ basic-authentication/src/Public.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Container from './Container'
+function Public() {
+    return (
+        <Container>
+            <h1>Public route</h1>
+        </Container>
+    )
+}
+export default Public
